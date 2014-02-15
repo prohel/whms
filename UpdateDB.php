1eb59c2f70a6204b9852d6b8a78f33377431165e
Marco Varela <mv.varela@gmail.com>
2014-02-15 14:14:27 -0500

    updtated parameters to db.php and added 'ADDPOLL' request handling

--- UpdateDB.php
+++ UpdateDB.php
@@ -8,12 +8,51 @@ if (mysqli_connect_errno())
   }
 $username = $email = $ideaname = $description = NULL;
 
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-   $username = test_input($_POST['username']);
-   $email = test_input($_POST['email']);
-   $idea = test_input($_POST["ideaname"]);
-   $description = test_input($_POST["description"]);
+  $jsonRequest = $_REQUEST["REQUEST"];
+  if ($jsonRequest !== "")
+  {
+    if ($jsonRequest.command == "ADDPOLL")
+    {
+      $count = $jsonRequest.count;
+      $usercode = "U" . random_Alpha(4);
+      $admincode = "A" . random_Alpha(4);
+      // Build the SQL command string for room
+      $sqlForm = "INSERT INTO rooms (count, usercode, admincode, created_at)
+  VALUES ('$count', '$usercode', '$admincode', NOW())";
+      pushToDB($sqlForm);
+      $roomID = mysql_insert_id(); 
+      // Build SQL string for each question
+      for ($i = 0; $i < $digit; $i++) 
+      {
+        $description = $jsonRequest.questions[i].description;
+        $choicea = $jsonRequest.questions[i].choicea;
+        $choiceb = $jsonRequest.questions[i].choiceb;
+        $choicec = $jsonRequest.questions[i].choicec;
+        $choiced = $jsonRequest.questions[i].choiced;
+        $choicee = $jsonRequest.questions[i].choicee;
+        $sqlQuestion = "INSERT INTO questions (room_id, description, choicea, choiceb, choicec, choiced, choicee, created_at)
+  VALUES ('$roomID', '$description', '$choicea', '$choiceb', '$choicec', '$choiced', '$choicee', NOW())";;
+        pushToDB($sqlQuestion);
+      }
+    else if ($jsonRequest.command == "ADDPOLL")
+    {
+
+    }
+
+  }
+}
+
+function random_Alpha($digit){
+  $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  $string = '';
+  for ($i = 0; $i < $digit; $i++) {
+    $string .= $characters[rand(0, strlen($characters) - 1)];
+  }
+  return $string;
 }
 
 function test_input($data)
@@ -24,13 +63,16 @@ function test_input($data)
    return $data;
 }
 
-$sql = "INSERT INTO rooms (user_id, name, description, category_id, created_at)
-VALUES (1, '$ideaname', '$description', 1, NOW())";
+function pushToDB($sql)
+{
+/*$sql = "INSERT INTO rooms (user_id, name, description, category_id, created_at)
+  VALUES (1, '$ideaname', '$description', 1, NOW())"; */
 
-if (!mysqli_query($mysql_link,$sql))
-  {
-die('Error: ' . mysqli_error($mysql_link));
-  }
-echo "1 record added";
+  if (!mysqli_query($mysql_link,$sql))
+    {
+  die('Error: ' . mysqli_error($mysql_link));
+    }
+  echo "1 record added";
+}
 
 ?>
