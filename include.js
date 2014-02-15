9671bf508d6c76e8fb6d2d9ab04feb65d569f909
Pierre <pierrerohel@gmail>
2014-02-15 14:16:41 -0500

    send to Marco

--- include.js
+++ include.js
@@ -13,6 +13,27 @@ var JSONObject = {
   "choice e": form.elements["Answer E"].value
 };
 questions[questionNumber] = JSONObject;
+questionNumber+=1;
 form.reset();
-return false;
+};
+
+function sendToMarco() {
+storequestion();
+for(int i = 1; i <= questionNumber; i++)
+var JSONObject = {
+  "command":"ADDPOLL",
+  "count":questionNumber,
+  "questions":questions
+};
+var xmlhttp;
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById('').innerHTML=JSON.parse(xmlhttp.responseText); //need to print user code and admin code
+    }
+  }
+  xmlhttp.open("POST","UpdateDB.php?REQUEST=" + JSONObject,true);
+  xmlhttp.send();
 };
