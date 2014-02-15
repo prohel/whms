5eae98b3d0c46df059b23d77b404ae51e01a9dc2
nevern00b <reyana.fayyaz@gmail.com>
2014-02-15 14:07:21 -0500

    html/css/js updated files- combining pierres and reyanas

--- include.js
+++ include.js
@@ -1,6 +1,7 @@
 
 questionNumber = 0;
 questions = new Array();
+pollOpen = 'Open';
 
 function storequestion() {
 var form = document.getElementById("question");
@@ -16,3 +17,36 @@ questions[questionNumber] = JSONObject;
 form.reset();
 return false;
 };
+
+function createPollPage() {
+    headingElement = document.getElementById('title'); //assign heading element
+    headingElement.innerHTML = 'Create A Poll'; //change heading element
+
+    eventId = document.getElementById('event');
+    eventId.innerHTML = ''; //clear student div
+
+    createNewId = document.getElementById('professor');
+    createNewId.innerHTML = ''; //clear prof div
+
+    questionTextBoxId = document.getElementById("questionTextBox");
+    questionTextBoxId.innerHTML = 'Insert Question Text Here:<form><textarea name="Question" style="height:200px;width:40%"></textarea><br><br>';
+
+    answerTextBoxId = document.getElementById("answerTextBox");
+    answerTextBoxId.innerHTML = 'A: <input type="text" name="Answer A"><br>B: <input type="text" name="Answer B"><br>C: <input type="text" name="Answer C"><br>D: <input type="text" name="Answer D"><br>E: <input type="text" name="Answer E"><br></form>'; 
+}
+
+function createStudentPage() {
+    headingElement = document.getElementById('title'); //assign heading element
+    headingElement.innerHTML = 'Poll is ' + pollOpen; //change heading element
+
+    eventId = document.getElementById('event');
+    eventId.innerHTML = ''; //clear student div
+
+    createNewId = document.getElementById('professor');
+    createNewId.innerHTML = ''; //clear prof div
+
+    answerOptionId = document.getElementById('event');
+    answerOptionId.innerHTML = '<h2>Answer the Question</h2><button name="A" type="button">A</button><br><button name="B" type="button">B</button> <br><button name="C" type="button">C</button> <br><button name="D" type="button">D</button> <br><button name="E" type="button">E</button> <br>';
+
+
+}
